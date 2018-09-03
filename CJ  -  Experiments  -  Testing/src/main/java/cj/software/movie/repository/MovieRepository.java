929d4252ed9bb6cf8be9c2b2b09823d0e90344ff
cjacobme <cjacobme@aol.com>
2018-09-03 21:17:57 +0200

    added search methods

--- CJ  -  Experiments  -  Testing/src/main/java/cj/software/movie/repository/MovieRepository.java
+++ CJ  -  Experiments  -  Testing/src/main/java/cj/software/movie/repository/MovieRepository.java
@@ -1,5 +1,7 @@
 package cj.software.movie.repository;
 
+import java.util.Optional;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,6 +18,13 @@ public class MovieRepository
 	{
 		Movie lMerged = this.entityManager.merge(pMovie);
 		return lMerged.getId();
+	}
+
+	public Optional<Movie> findById(Long pMovieId)
+	{
+		Movie lFound = this.entityManager.find(Movie.class, pMovieId);
+		Optional<Movie> lResult = Optional.ofNullable(lFound);
+		return lResult;
 	}
 
 }
--- CJ  -  Experiments  -  Testing/src/test/java/cj/software/movie/repository/MovieRepositoryTest.java
+++ CJ  -  Experiments  -  Testing/src/test/java/cj/software/movie/repository/MovieRepositoryTest.java
@@ -4,8 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import javax.persistence.EntityManager;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -21,30 +24,62 @@ public class MovieRepositoryTest
 	@Mock
 	private EntityManager entityManager;
 
+	private MovieRepository repository;
+
+	@Before
+	public void setupRepository()
+	{
+		this.repository = new MovieRepository();
+		this.repository.entityManager = this.entityManager;
+	}
+
 	@Test
 	public void saveMovie()
 	{
-		Movie lNewMovie = this.createMovie();
-		Movie lSavedMovie = this.createMovie();
-		lSavedMovie.setId(MOVIE_ID);
+		Movie lNewMovie = this.createMovie(false);
+		Movie lSavedMovie = this.createMovie(true);
 
 		when(this.entityManager.merge(lNewMovie)).thenReturn(lSavedMovie);
 
-		MovieRepository lRepo = new MovieRepository();
-		lRepo.entityManager = this.entityManager;
-
-		long lReturnedId = lRepo.save(lNewMovie);
+		long lReturnedId = this.repository.save(lNewMovie);
 
 		verify(this.entityManager).merge(lNewMovie);
 		assertThat(lReturnedId).as("returned id").isEqualTo(MOVIE_ID);
 	}
 
-	private Movie createMovie()
+	@Test
+	public void findExistingMovie()
+	{
+		Movie lReturnedMovie = this.createMovie(true);
+		when(this.entityManager.find(Movie.class, MOVIE_ID)).thenReturn(lReturnedMovie);
+
+		Optional<Movie> lReturned = this.repository.findById(MOVIE_ID);
+		verify(this.entityManager).find(Movie.class, MOVIE_ID);
+		assertThat(lReturned)
+				.isNotNull()
+				.hasValue(lReturnedMovie)
+				.usingFieldByFieldValueComparator();
+	}
+
+	@Test
+	public void findNonExistingMovie()
+	{
+		when(this.entityManager.find(Movie.class, MOVIE_ID)).thenReturn(null);
+		Optional<Movie> lReturned = this.repository.findById(MOVIE_ID);
+		verify(this.entityManager).find(Movie.class, MOVIE_ID);
+		assertThat(lReturned).isNotNull().isNotPresent();
+	}
+
+	private Movie createMovie(boolean pWithId)
 	{
 		Movie lResult = new Movie();
 		lResult.setTitle("Das Boot");
 		lResult.setDirector("Wolfgang Petersen");
 		lResult.setReleaseYear(1981);
+		if (pWithId)
+		{
+			lResult.setId(MOVIE_ID);
+		}
 		return lResult;
 	}
 }
